d6de06c2e7a3dc10ebbfb562a6f2c27b46c2dad6
Chris Leonard <lenwood.cc@gmail.com>
2014-09-27 20:35:43 -0500

    convert json to sqlite database

---
+++ Python/json-to-sqlite.py
@@ -0,0 +1,27 @@
+import json, sqlite3, sys
+
+# pass the filename as an argument when calling this script
+filename_in = sys.argv[1]
+filename_list = [filename_in.split('.')[0], 'db']
+filename_out = ".".join(filename_list)
+
+# read in the filename
+input = open(filename_in)
+jsonData = json.load(input)
+input.close()
+
+# structure the json data
+jsonColumns = str(",".join(list(jsonData[0].keys())))
+jsonValues = [':{0}'.format(i) for i in jsonData[0].keys()]
+jsonValues = ", ".join(jsonValues)
+
+# assemble the SQL query
+query = ["INSERT INTO jsonFile VALUES (", jsonValues, ")"]
+queryString = " ".join(query)
+
+# establish connection to the database & execute the query
+conn = sqlite3.connect(filename_out) # or use :memory: to put it in RAM
+with conn:
+	cur = conn.cursor()
+	cur.execute("CREATE TABLE IF NOT EXISTS logfile(" + jsonColumns + ")")
+	cur.executemany(queryString, jsonData)
