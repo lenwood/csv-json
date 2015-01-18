f52f17d2ed201099cd3e76516c1155719577fbfb
Chris Leonard <lenwood.cc@gmail.com>
2015-01-18 01:12:13 -0600

    Delete xml-to-csv.ps1

--- PowerShell/xml-to-csv.ps1
+++
@@ -1,8 +0,0 @@
-# to run ./xml-to-csv.ps1 filename.xml
-
-param([string]$file)
-$filename_list = $file.Split(".")[0], "csv"
-$filename_out = $filename_list -join '.'
-
-$data = Get-Content $file
-$data.Sites.ChildNodes | Export-Csv $filename_out -NoTypeInformation -Delimiter:"," -Encoding:UTF8
